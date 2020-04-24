09b41716a58a31c4c5b9dc39243a2924e6ed7208
Gunnar R <gunnarrosenberg@gmail.com>
2020-04-23 20:12:39 -0700

    up

--- lib/test_app.ex
+++ lib/test_app.ex
@@ -15,4 +15,41 @@ defmodule TestApp do
   def hello do
     :world
   end
+
+  def create_zoo do
+    ["lion", "tiger", "gorilla", "elephant", "monkey", "giraffe"]
+  end
+
+  def randomise(zoo) do
+    Enum.shuffle(zoo)
+  end
+
+  def contains?(zoo, animal) do
+    Enum.member?(zoo, animal)
+  end
+
+  def see_animals(zoo, count) do
+    {_seen, to_see} = Enum.spilt(zoo, -count)
+    to_see
+  end
+
+  def save(zoo, filename) do
+    binary = :erlang.term_to_binary(zoo)
+    File.write(filename, binary)
+  end
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
